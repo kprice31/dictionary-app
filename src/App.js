3a9eacfa3e7bb620d414f58a8438d4785cdc0fde
kprice31 <54342102+kprice31@users.noreply.github.com>
2021-09-11 16:19:17 -0600

    starting search engine

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
 import "./App.css";
+import logo from "./logo.png";
+import Dictionary from "./Dictionary.js";
 
-function App() {
+export default function App() {
   return (
     <div className="App">
-      <header className="App-header">Hello</header>
+      <div className="container">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+        </header>
+        <main>
+          <Dictionary />
+        </main>
+        <footer className="App-footer">
+          <small>Coded by Kelsi Price</small>
+        </footer>
+      </div>
     </div>
   );
 }
-
-export default App;
---
+++ src/Dictionary.js
@@ -0,0 +1,27 @@
+import React, { useState } from "react";
+
+export default function Dictionary() {
+  let [keyword, setKeyword] = useState("");
+
+  function handleKeywordChange(event) {
+    setKeyword(event.target.value);
+  }
+
+  function search(event) {
+    event.preventDefault();
+    alert(`Searching for "${keyword}"...`);
+  }
+
+  return (
+    <div className="Dictionary">
+      <form onSubmit={search}>
+        <input
+          type="search"
+          onChange={handleKeywordChange}
+          placeHolder="Search"
+          autoFocus={true}
+        />{" "}
+      </form>
+    </div>
+  );
+}
