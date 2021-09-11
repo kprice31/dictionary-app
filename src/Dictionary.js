3f7b45815b217c3d965aa06155416e343172db49
kprice31 <54342102+kprice31@users.noreply.github.com>
2021-09-11 17:18:35 -0600

    created new state for results...input the definitions on the screen

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,16 +1,18 @@
 import React, { useState } from "react";
 import axios from "axios";
 import "./Dictionary.css";
+import Results from "./Results";
 
 export default function Dictionary() {
   let [keyword, setKeyword] = useState("");
+  let [results, setResults] = useState(null);
 
   function handleKeywordChange(event) {
     setKeyword(event.target.value);
   }
 
   function handleResponse(response) {
-    console.log(response.data[0]);
+    setResults(response.data[0]);
   }
 
   function search(event) {
@@ -31,6 +33,7 @@ export default function Dictionary() {
           autoFocus={true}
         />{" "}
       </form>
+      <Results results={results} />
     </div>
   );
 }
